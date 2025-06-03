a20dac35c3568fadd16a35e42a115a31b48a19f4
jek9356 <devjakariya9356@gmail.com>
2025-06-03 11:46:06 +0600

    object and object methods completed

--- JS25/Intermediate/advanced-array.js
+++ JS25/Intermediate/advanced-array.js
@@ -1,5 +1,5 @@
 /***
- * Mutator Array Methods 
+ * Mutator Array Methods 10 
  * @param { push () } add elements to the end
  * @param { unshift() } add elements to the beginning
  * @param { pop() } remove the last element
@@ -146,11 +146,192 @@ let formFileds = new Array(5).fill('');
 
 let row = ['A1', 'B1', 'C1', 'D1','F1'];
     row.copyWithin(2, 0, 2);
-    console.log(row);
+    // console.log(row);
 
 let userRow = ['Name', 'Email', 'Phone', 'Address','age', 'education'];
 let clonedRow = userRow.copyWithin(2, 0, 3);
-console.log(userRow);
+// console.log(userRow);
+
+
+/***
+ * Accessor Array Methods Seven
+ * @param { concat() } Merge arrays 
+ * @param { slice() } Extract a section
+ * @param { join() } Convert array to string 
+ * @param { includes() } Check if a value exists 
+ * @param { indexOf() } Get index of first Occurrence
+ * @param { LastIndexOf() } Get index of last occurrence 
+ * @param { toString() } Convert array to comma-separated string
+ * @param {at() } Access element at specific index(including negative index)
+ * 
+ */
+
+
+/****
+ * @param { concat() }
+ * @param concat() marge data from different source
+ * @param use case dashboard apps, analytics tools,
+ * 
+ */
+
+const usersFromAPI = ['Rumon', 'Rasel', 'Alexa'];
+const usersFromDB = ['Jek', 'Jakariya', 'Alex'];
+
+const margeUsers = usersFromAPI.concat(usersFromDB); 
+
+// console.log(margeUsers[0]);
+
+
+/***
+ * @param { slice(startIndex, endIndex) }  Extract a portion of an array 
+ * @param use case pagination 
+ */
+
+let portion = fruits.slice(2, 5);
+
+const posts = ['Post1', 'Post2', 'Post3', 'Post4', 'Post5', 'Post6', 'Post7', 'Post8']
+
+function pagination(array, page = 1, pageSize = 3) {
+    // defaule page = 1; start = (index:1 - 1 = 0), (index:2 - 1 = 1).. * pageSize: 3;
+    const start = (page - 1) * pageSize; 
+    const end = page * pageSize;
+    return array.slice(start, end);
+}
+/**
+ * how it work?
+ * Page = 1;
+ * start = (1 - 1) * 3 = 0;
+ * end = 1 * 3 = 3;
+ * 
+ * Page = 2; 
+ * start = (2 - 1) * 3 = 3;
+ * end = 2 * 3 = 6;
+ * 
+ * Page = 3;
+ * start = (3 - 1) * 3 = 6;
+ * end = 3 * 3 = 9; 
+ */
+
+// console.log(pagination(posts));
+
+
+/***
+ * @param { joiin() }  convert array to string 
+ * 
+ */
+
+let words = ['Hello', 'World'];
+let sentence = words.join(' ');
+
+const tags = ["JavaScript", "PHP", "Node.js", "React.js"];
+const tagSeperation = tags.join(', ');
+// console.log(tagSeperation);
+
+
+/***
+ * @param { includes(value, startIndex) } check conditional statement
+ * @param includes() return boolean true or false
+ * 
+ */
+
+
+const appRole = ['Super Admin', 'Admin', 'Author', 'User']
+const checkRole = appRole.includes('Author');
+
+if(checkRole) {
+    // console.log("Admin Access Granted !");
+}else {
+    // console.log("Acces Denied");
+}
+
+/***
+ * @param {string} currentUserRole - The role of current user (e.g., 'Admin', 'Super Admin')
+ * @param {string[]}  allowedRoles - Array of roles allowed to accees a resources 
+ * @returns {boolean} - Returns true if the user has access, otherwise false. 
+ * 
+ * @example
+ * hasAccess('Admin', ['Admin', 'Super Admin'])
+ */
+
+// pro level 
+const allowedRoles = ['admin', 'super admin', 'seller'];
+const currentUserRole = 'admin';
+const multiCurrentRoles = ['admin', 'editor', 'seller']; //user has multiple roles
+
+function hasAccess(currentUserRole, allowedRoles) {
+    return allowedRoles.includes(currentUserRole);
+}
+
+if (hasAccess(currentUserRole, allowedRoles)) {
+    // console.log(currentUserRole + " " +"Access Granted !");
+}else {
+    // console.log( currentUserRole + ' ' +"Access Denied !");
+}
+
+//second way it is slow for  
+function accessControl(allowedRoles, currentUserRole) {
+    return allowedRoles.filter(role => role === currentUserRole);
+}
+
+let matchRole = accessControl(allowedRoles, currentUserRole);
+if(matchRole.length > 0) {
+    // console.log(`${matchRole} Access Granted`);
+}else {
+    // console.log('Access Denied !');
+}
+
+/***
+ * @param { filter() and includes() } - check user multiple roles
+ * @param { find() } - check user multiple roles 
+ */
+//this use case include() efficient
+let currentRoles = multiCurrentRoles.filter(role => allowedRoles.includes(role));
+// this case find() slightly slower
+// let currentRoles = multiCurrentRoles.filter(role => allowedRoles
+        // .find(allowed => allowed === role));     
+
+if (currentRoles.length > 0) {
+    // console.log(`${currentRoles} Access Granted !`);
+}else {
+    // console.log(`Access Denied !`);
+}
+
+
+/**
+ * @param { indexOf() } - specific index of array
+ * @param use case - validation, removal, or highlighting Element
+ * 
+ **/ 
+
+const indexFind = fruits.indexOf('Apple');
+// console.log(indexFind);
+
+// lastIndexOf()
+const lastIndex = fruits.lastIndexOf('Apple');
+// console.log(lastIndex);
+
+//toString()
+const fruitString = fruits.toString();
+// console.log(fruitString);
+
+//at()
+
+// console.log(allowedRoles.at(0));
+// console.log(allowedRoles.at(-1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ JS25/Intermediate/object-methods.js
@@ -0,0 +1,110 @@
+/***
+ * @param object mehtods
+ * @param { object.keys() } - return array of all keys in the object
+ * @param { object.values(obj) } - return array of all values in the object
+ * @param { object.entries() } - return key-value pairs as nested arrays
+ * @param { object.assign(target, source) } - copies properties from sourse object(s) to target
+ * @param { object.freeze(obj) } - prevents any changes to an object(immutable)
+ * @param { object.seal(obj)} - prevents adding/deleting but allows modifying existing ones.
+ * @param { object.hasOwnPropery(key) } - checks property in object
+ * @param { object.defineProperty() } -  Define or modify a property with fine control (e.g., non-enumerable, non-writable)
+ * 
+ */
+
+// object 
+const user = {
+    name: 'Jakariya',
+    age: 26,
+    isDeveloper: true,
+    greet: function() {
+        console.log(`Hello, ${this.name}`);
+    }
+}
+
+// console.log(user.greet());
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+const assignObject = {
+    married: 'Single', 
+    country: 'Bangladesh', 
+    email: 'jek@gmail.com'
+    };
+
+const marge = Object.assign({}, user, assignObject);
+//freeze not change anything
+Object.freeze(user);
+user.age = 23;
+// console.log(user);
+
+//only modify existin onces but can't adding/deleting
+Object.seal(user);
+user.name = 'Jakariya Aman';
+user['division'] = 'Sylhet';
+delete user.age;
+//professional assign object
+
+function assignRole(userObj, role) {
+    // userObj.role = role;
+    userObj['role'] = role;
+    return userObj;
+}
+let assignNew = assignRole(assignObject, 'Admin');
+
+//Object.hasOwnProperty(key)
+let checkPropery = user.hasOwnProperty('age');
+
+// Object.defineProperty
+Object.defineProperty(user, 'id', {
+    value: 101,
+    writable: false,
+    enumerable: true
+});
+user.id = 222;
+console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
