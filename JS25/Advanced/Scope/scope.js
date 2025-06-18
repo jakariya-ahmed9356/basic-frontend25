29fd1cffdfc38691f9b7b9e74f7b5c2f1708a931
jek9356 <devjakariya9356@gmail.com>
2025-06-18 11:09:12 +0600

    completed closure

--- JS25/Advanced/Scope/scope.js
+++ JS25/Advanced/Scope/scope.js
@@ -1,3 +1,50 @@
+// JS Scopes
+function outer() {
+    let name = "Adam Smith";
+    function inner() {
+        console.log("Hello " + name);
+    }
+
+    return inner;
+
+}
+
+// const greet = outer();
 
 
+// Simulate private variables
+function counter() {
+    let count = 0;
 
+    return {
+        increment: function () {
+            count++;
+            console.log(count);
+        },
+        decrement: function() {
+            count--;
+            console.log(count);
+        },
+    };
+}
+
+const myCounter = counter();
+myCounter.increment();
+myCounter.increment();
+myCounter.decrement();
+// console.log(greet());
+
+// auto save closer function
+function autoSave(interval) {
+    let timer;
+    return function startSaving() {
+        if (!timer) {
+            timer = setInterval(() => {
+                console.log('Saving draft');
+            }, interval);
+        }
+    }
+}
+
+const startDraftSave = autoSave(300);
+startDraftSave(); // start saving every 3 seconds
