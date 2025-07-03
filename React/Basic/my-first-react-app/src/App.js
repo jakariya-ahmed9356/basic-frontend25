b3cc5681fd1128997c1002d11561616d0aa009c9
jek9356 <devjakariya9356@gmail.com>
2025-07-03 16:21:55 +0600

    react installed and basic component completed

---
+++ React/Basic/my-first-react-app/src/App.js
@@ -0,0 +1,12 @@
+import Welcome from './welcome';
+import WelcomeClass from './WelcomeClass';
+function App() {
+  return (
+    <div>
+      <Welcome/>
+      <WelcomeClass/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React/Basic/my-first-react-app/src/WelcomeClass.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class WelcomeClass extends Component {
+    render() {
+        return <h1>Welcome form class Component!</h1>
+    }
+}
+
+export default WelcomeClass;
