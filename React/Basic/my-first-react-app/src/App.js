336249b1abf05976587f0eeaa5b10e48017d13b2
jek9356 <devjakariya9356@gmail.com>
2025-07-08 01:26:17 +0600

    basic react completed

--- React/Basic/my-first-react-app/src/App.js
+++ React/Basic/my-first-react-app/src/App.js
@@ -1,4 +1,8 @@
-import Element, {ParentDiv, Bye } from './welcome';
+import Element, {
+  ParentDiv, Bye, Calculate,LogIn,ElementDesign,
+  RenderList, Card
+}
+from './welcome';
 import WelcomeClass from './WelcomeClass';
 
 function App() {
@@ -7,6 +11,12 @@ function App() {
       <Element />
       <ParentDiv />
       <Bye />
+      <Calculate/>
+      <LogIn/>
+     <ElementDesign/>
+     <RenderList/>
+     <Card title="Practise First Props in React" desc="learning new react props" id="39393"/>
+     <Card title="Practise First Props in React 2" desc="learning new react props 2" id="39393"/>
     </div>
   );
 }
--- React/Basic/my-first-react-app/src/welcome.js
+++ React/Basic/my-first-react-app/src/welcome.js
@@ -20,5 +20,64 @@ function ParentDiv() {
 function Bye() {
     return <h2>GoodBye! </h2>
 }
+
+function Calculate() {
+    return <h2> 2 + 2 = {2 + 2} </h2>
+}
+
+function LogIn() {
+    const loggedIn = true;
+    return (
+        <div>
+            {loggedIn ? <h2> Welcome Back ! </h2> : <h1>Please log in!</h1>}
+        </div>
+    );
+}
+
+function ElementDesign() {
+    const styles = {
+        colo: "red",
+        fontSize: "30px",
+        background: "gray",
+    }
+
+    return <h2 style={styles}>Styled Heading</h2>
+}
+
+function RenderList() {
+    const fruits = ["Apple", 'Banana','Orange', 'Mango'];
+    return (
+        <ul>
+            <h2>Fruits Items:</h2>
+            {fruits.map((fruit, index) => (
+                <li key={index}>{fruit}</li>
+            ))}
+        </ul>
+    );
+}
+
+// props
+function Card(props) {
+    const style = {
+        background: 'orange',
+        color: 'white',
+        padding: '15px',
+        margin: '10px',
+        }
+
+    return (
+        <div style={style}>
+            <h3>{props.title}</h3>
+            <p>{props.desc}</p>
+            <a href={props.id}> Read more.. </a>
+        </div>
+    );
+}
+
 export default Element;
-export {ParentDiv, Bye };
+export {
+    ParentDiv, Bye, Calculate, 
+    LogIn, ElementDesign, RenderList,
+    Card
+
+};
