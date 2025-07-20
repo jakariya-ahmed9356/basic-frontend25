ae07029f8041d5f4e0ee6b51dad6b3ed4e344b0c
jek9356 <devjakariya9356@gmail.com>
2025-07-20 22:08:35 +0600

    start itnermediate react

---
+++ React/intermediate/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-orange-500'>Intermediate Lever React Learning</h1>
+    </div>
+  )
+}
+export default App
