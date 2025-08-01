0c51f84013f5e8874d8323489d16c716845481ae
jek9356 <devjakariya9356@gmail.com>
2025-08-01 22:04:54 +0600

    variables declaration in tailwind

--- Tailwin25/TailwindReactBasic/src/components/ThemeCom.jsx
+++ Tailwin25/TailwindReactBasic/src/components/ThemeCom.jsx
@@ -1,14 +1,29 @@
 export default function Theme() {
     return (
-        <div className="bg-[var(--color-mint-500)]">
-            <h1 className="">Theme Color</h1>
+        <div className="">
+            <div className="grid grid-cols-1 space-y-4 sm:grid-cols-2 md:grid-cols-3">
+                <h1 className="bg-primary">Theme Color</h1>
 
-            <div className="bg-custom-400">
-                <h2 className="text-3xl">using Theme by style </h2>
+                <div className="bg-custom-400">
+                    <h2 className="text-3xl">using Theme by style </h2>
+                    <button className="btn hover:bg-gray-500 hover:text-white transition">Click Me</button>
+                </div>
+
+
+                <div className="card">
+                    <h2>Card Heading</h2>
+                </div>
+                
+            </div>
+            
+            <div
+            className="max-w-md h-20 mt-4 m-auto card fade-in-scale"
+            
+            >
+            <h2>Animation</h2>
             </div>
 
-            <button className="btn hover:bg-gray-500 hover:text-white transition">Click Me</button>
         </div>
-
+        
     );
 }
