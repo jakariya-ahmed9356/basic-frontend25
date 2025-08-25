5b1ece5addf6c9186d4968c4cf8ecdd77b04237b
jek9356 <devjakariya9356@gmail.com>
2025-08-25 22:59:27 +0600

    view data

--- JS25/BasicJS/localStorage.js
+++ JS25/BasicJS/localStorage.js
@@ -6,10 +6,19 @@ const user = [
 
 
 
+// localStorage.setItem('user', JSON.stringify(user));
+
+user.push({
+    name: 'Jakariya Aman', age: 24, desi: 'Backend Developer'
+});
+
 localStorage.setItem('user', JSON.stringify(user));
+let removeName = localStorage.getItem('name');
+localStorage.removeItem(removeName);
 
 const getUser = JSON.parse(localStorage.getItem('user'));
-
+console.log(getUser);
 document.getElementById('userInfo')
 .innerHTML = getUser.map(user => `<li>Name: ${user.name }, Age: ${user.age}, Designation: ${user.desi}</li>`)
     .join('');
+
